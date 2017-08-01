b76e930ef0a7f4769d23a1673e939735fe75bb15
BenjiBong <22+BenjiBong@users.noreply.github.com>
2017-08-01 08:35:28 +0800

    First upload of whole project from Ben

---
+++ app/Http/Controllers/PetsController.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;//to be able to delete image. For storage:: library
+use App\Pet;//to use eloquent
+use Illuminate\Support\Facades\Auth;
+
+class PetsController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+      //
+      // $pets = Post::all();
+      // $pets = Post::where('title', 'First Post')->get();
+      //$pets=DB::select('SELECT * FROM posts'); to use sql
+      //$pets = Post::orderBy('created_at', 'asc')->take(1)->get();//to limit to just one post
+      //$pets = Post::orderBy('created_at', 'asc')->get();
+
+      if (!Auth::user()){
+        return redirect('/')->with('error', 'You need to login!');
+      }
+
+      $pets = Pet::orderBy('created_at', 'asc')->paginate(10);//paginate with 10 per page
+      return view('pets.index')->with('pets', $pets);
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+      return view('pets.create');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+      $this->validate($request, [
+          'name' => 'required',
+          'type' => 'required',
+          'color' => 'required',
+          'pet_image' => 'image|nullable|max:1999',
+      ]);
+
+      //Handle file upload
+            if($request->hasFile('pet_image')){
+              //Get filename with extension
+              $filenameWithExt = $request->file('pet_image')->getClientOriginalName();
+              //Get just file name
+              $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+              //Get just extension
+              $extension = $request->file('pet_image')->getClientOriginalExtension();
+              //Filename to Store
+              $filenameToStore = $filename.'_'.time().'.'.$extension;
+
+              $path = $request->file('pet_image')->storeAs('public/pet_images', $filenameToStore);
+            }
+            else {
+              $filenameToStore = 'noimage.jpg';
+            }
+            //to create pet
+            $pet = new Pet;
+            $pet->name = $request->input('name');
+            $pet->type = $request->input('type');
+            $pet->color = $request->input('color');
+            $pet->user_id = auth()->user()->id;
+            $pet->pet_image = $filenameToStore;
+            $pet->save();
+
+            return redirect('/pets')->with('success', 'Pet Saved');
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+      //
+      $pet = Pet::find($id);
+      return view('pets.show')->with('pet', $pet);
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $pet = Pet::find($id);
+
+    if (!Auth::user()){
+      return redirect('/')->with('error', 'You need to login!');
+    }
+    if (auth()->user()->id != $pet->user_id){
+      return redirect('/pets')->with('error', 'You do not have permission to edit this pet!');
+    }
+      return view('pets.edit')->with('pet', $pet);
+      //$pets = Pet::orderBy('created_at', 'asc')->paginate(10);//paginate with 10 per page
+      //return view('pets.index')->with('pets', $pets);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $this->validate($request, [
+        'name' => 'required',
+        'type' => 'required',
+        'color' => 'required',
+        'pet_image' => 'image|nullable|max:1999',
+    ]);
+
+    //Handle file upload
+          if($request->hasFile('pet_image')){
+            //Get filename with extension
+            $filenameWithExt = $request->file('pet_image')->getClientOriginalName();
+            //Get just file name
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            //Get just extension
+            $extension = $request->file('pet_image')->getClientOriginalExtension();
+            //Filename to Store
+            $filenameToStore = $filename.'_'.time().'.'.$extension;
+
+            $path = $request->file('pet_image')->storeAs('public/pet_images', $filenameToStore);
+          }
+
+          //to create pet
+          $pet = Pet::find($id);
+          $pet->name = $request->input('name');
+          $pet->type = $request->input('type');
+          $pet->color = $request->input('color');
+          if($request->hasFile('pet_image')){
+            $pet->pet_image = $filenameToStore;
+
+          }
+          
+          $pet->save();
+
+          return redirect('/pets')->with('success', 'Pet Updated');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+      $pet = Post::find($id);
+      $pet->delete();
+      return redirect('/posts')->with('success', 'Post Removed');
+  }
+}
---
+++ resources/views/pets/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <a href="/pets" class="btn btn-default">Go back</a>
+    <br>
+    <br>
+    <div class="row">
+      <div class="col-md-4 col-sm-4">
+        <img style="width:100%" src="/storage/pet_images/{{$pet->pet_image}}" alt="">
+      </div>
+
+    </div>
+    <h1>{{$pet->name}}</h1>
+    <div><p>Type: {{$pet->type}}</p></div>
+    <div><p>Color: {{$pet->color}}</p></div>
+    <div><p>Owner: {{$pet->user->name}}</p></div>
+
+         <hr>
+       <small>Created at: {{$pet->created_at}}</small><br>
+        <small>Last updated: {{$pet->updated_at}}</small>
+        <hr>
+        <a href="/pets/{{$pet->id}}/edit" class="btn btn-default">Edit</a>
+
+        {!!Form::open(['action' => ['PetsController@destroy', $pet->id], 'method' => 'pet', 'class' => 'pull-right' ])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+        {!!Form::close()!!}
+@endsection
