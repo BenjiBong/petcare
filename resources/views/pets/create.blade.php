a4d263bfd45eb083b6f1da5557f6734fd9fcf2ea
OneM <ben.bong95@gmail.com>
2017-09-15 14:54:07 +0800

    Added red * css styling and html for required fields

--- resources/views/pets/create.blade.php
+++ resources/views/pets/create.blade.php
@@ -5,17 +5,17 @@
     <div class="row">
       <div class="col-md-4 col-sm-4">
         {!! Form::open(['action'=>'PetsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        <div class="form-group">
-            {{Form::label('name', 'Name')}}
-            {{Form::text('name', '', ['class' => 'form-control', 'placeholder'=>'Name'])}}
+        <div class="form-group required">
+            {{Form::label('name', 'Name', ['class' => 'control-label'])}}
+            {{Form::text('name', '', ['class' => 'form-control ', 'placeholder'=>'Name', 'required' => 'required'])}}
         </div>
-        <div class="form-group">
-            {{Form::label('type', 'Type')}}
-            {{Form::text('type', '', ['class' => 'form-control', 'placeholder'=>'Type'])}}
+        <div class="form-group required">
+            {{Form::label('type', 'Type', ['class' => 'control-label'])}}
+            {{Form::text('type', '', ['class' => 'form-control', 'placeholder'=>'Type', 'required' => 'required'])}}
         </div>
-        <div class="form-group">
-            {{Form::label('color', 'Color')}}
-            {{Form::text('color', '', ['class' => 'form-control', 'placeholder'=>'Color'])}}
+        <div class="form-group required">
+            {{Form::label('color', 'Color', ['class' => 'control-label'])}}
+            {{Form::text('color', '', ['class' => 'form-control', 'placeholder'=>'Color', 'required' => 'true'])}}
         </div>
         <div class="form-group">
                 {{Form::file('pet_image')}}
@@ -23,6 +23,9 @@
 
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
         {!! Form::close() !!}
+        <div class="form-group required">
+          <label class='control-label'></label>Required.
+        </div>
       </div>
 
     </div>
