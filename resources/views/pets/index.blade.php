81e36c22f166f226a7c60c552cf4400aef679201
OneM <ben.bong95@gmail.com>
2017-09-19 00:17:39 +0800

    Ben's changes merged with Kinah's

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -4,12 +4,12 @@
     <h1>Pets</h1>
     @if (count($pets) > 0)
         @foreach($pets as $pet)
-        
+
             <div class = "col-sm-6 col-md-3">
                 <div class = "thumbnail">
                     <a href="/pets/{{$pet->id}}"><img src="/storage/pet_images/{{$pet->pet_image}}" alt="" style="max-height:250px max-width:500px" class="img-rounded">
-            
-            
+
+
                     <div class = "caption">
                         <h4 class="card-title"><a href="/pets/{{$pet->id}}">{{$pet->name}}</a></h4>
                         <h5 class="card-text">Type :{{$pet->type}}</h5>
@@ -18,13 +18,13 @@
                     </div>
                 </div>
             </div>
+
         @endforeach
         {{$pets->links()}}
     @else
         <p class="label label-warning">No pets found</p>
     @endif
+      <div class="row">
+        <a class="btn btn-default" href="/pets/create">New Pet</a>
+      </div>
 @endsection
-
-
-
-  
