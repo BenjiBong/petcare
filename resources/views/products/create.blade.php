824954614cc234491155165722876f3783b58aeb
OneM <ben.bong95@gmail.com>
2017-09-20 00:52:48 +0800

    Edited product forms

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,28 +2,30 @@
 
 @section('content')
     <h1>Add Product</h1>
+    <div class="form-group required">
+      <label class='control-label'></label>Indicates required fields.
+    </div>
 
-      
         {!! Form::open(['action'=>'ProductsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        <div class="form-group">
-            {{Form::label('title', 'Product Name')}}
+        <div class="form-group required">
+            {{Form::label('title', 'Product Name', ['class' => 'control-label'])}}
             {{Form::text('title', '', ['class' => 'form-control', 'placeholder'=>'Product Name'])}}
         </div>
-        <div class="form-group">
-            {{Form::label('body', 'Description')}}
+        <div class="form-group required">
+            {{Form::label('body', 'Description', ['class' => 'control-label'])}}
             {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder'=>'Product Description', 'id'=>'article-ckeditor'])}}
         </div>
-        <div class="form-group" step="any">
-            {{Form::label('price', 'Price')}}
-            
+        <div class="form-group required" step="any">
+            {{Form::label('price', 'Price', ['class' => 'control-label'])}}
+
             {!! Form::number('price',null,['class' => 'form-control','step'=>'any', 'placeholder'=>'Price']) !!}
         </div>
-        <div class="form-group">
-            {{Form::label('sku', 'SKU')}}
+        <div class="form-group required">
+            {{Form::label('sku', 'SKU', ['class' => 'control-label'])}}
             {{Form::text('sku', '', ['class' => 'form-control', 'placeholder'=>'Stock Keeping Unit'])}}
         </div>
-        <div class="form-group">
-            {{Form::label('stock', 'Stock')}}
+        <div class="form-group required">
+            {{Form::label('stock', 'Stock', ['class' => 'control-label'])}}
             {{Form::number('stock', '', ['class' => 'form-control', 'placeholder'=>'Stock'])}}
         </div>
         <div class="form-group">
@@ -32,8 +34,9 @@
 
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
         {!! Form::close() !!}
-      
 
-    
+
+
+
 
 @endsection
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -2,25 +2,28 @@
 
 @section('content')
     <h1>Edit Product</h1>
+    <div class="form-group required">
+      <label class='control-label'></label>Indicates required fields.
+    </div>
     {!! Form::open(['action' => ['ProductsController@update', $products->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-    <div class="form-group">
-        {{Form::label('title', 'Title')}}
+    <div class="form-group required">
+        {{Form::label('title', 'Title', ['class' => 'control-label'])}}
         {{Form::text('title', $products->title, ['class' => 'form-control', 'placeholder'=>'Title'])}}
     </div>
-    <div class="form-group">
-        {{Form::label('body', 'Body')}}
+    <div class="form-group required">
+        {{Form::label('body', 'Body', ['class' => 'control-label'])}}
         {{Form::textarea('body', $products->body, ['class' => 'form-control', 'placeholder'=>'Body', 'id'=>'article-ckeditor'])}}
     </div>
-    <div class="form-group" step="any">
-            {{Form::label('price', 'Price')}}
+    <div class="form-group required" step="any">
+            {{Form::label('price', 'Price', ['class' => 'control-label'])}}
             {!! Form::number('price',$products->price,['class' => 'form-control','step'=>'any', 'placeholder'=>'Price']) !!}
     </div>
-    <div class="form-group">
-            {{Form::label('sku', 'SKU')}}
+    <div class="form-group required">
+            {{Form::label('sku', 'SKU', ['class' => 'control-label'])}}
             {{Form::text('sku', $products->sku, ['class' => 'form-control', 'placeholder'=>'Stock Keeping Unit'])}}
     </div>
-    <div class="form-group">
-            {{Form::label('stock', 'Stock')}}
+    <div class="form-group required">
+            {{Form::label('stock', 'Stock', ['class' => 'control-label'])}}
             {{Form::number('stock', $products->stock, ['class' => 'form-control', 'placeholder'=>'Stock'])}}
     </div>
     <div class="form-group">
