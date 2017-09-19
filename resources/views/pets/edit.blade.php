034cb74f0cd37376add6e3a858ad028471a5d662
OneM <ben.bong95@gmail.com>
2017-09-20 00:45:38 +0800

    Edited required indicators for edit pet

--- resources/views/pets/edit.blade.php
+++ resources/views/pets/edit.blade.php
@@ -6,22 +6,25 @@
     <div class="row">
       <div class="col-md-4 col-sm-4">
         {!! Form::open(['action' => ['PetsController@update', $pet->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        <div class="form-group">
-            {{Form::label('name', 'Name')}}
-            {{Form::text('name', $pet->name, ['class' => 'form-control', 'placeholder'=>'Name'])}}
+        <div class="form-group required">
+            {{Form::label('name', 'Name', ['class' => 'control-label'])}}
+            {{Form::text('name', $pet->name, ['class' => 'form-control', 'placeholder'=>'Name', 'required' => 'required'])}}
         </div>
-        <div class="form-group">
-            {{Form::label('type', 'Type')}}
-            {{Form::text('type', $pet->type, ['class' => 'form-control', 'placeholder'=>'Type'])}}
+        <div class="form-group required">
+            {{Form::label('type', 'Type', ['class' => 'control-label'])}}
+            {{Form::text('type', $pet->type, ['class' => 'form-control', 'placeholder'=>'Type', 'required' => 'required'])}}
         </div>
-        <div class="form-group">
-            {{Form::label('color', 'Color')}}
-            {{Form::text('color', $pet->color, ['class' => 'form-control', 'placeholder'=>'Color'])}}
+        <div class="form-group required">
+            {{Form::label('color', 'Color', ['class' => 'control-label'])}}
+            {{Form::text('color', $pet->color, ['class' => 'form-control', 'placeholder'=>'Color', 'required' => 'required'])}}
         </div>
-        <div class="form-group">
+        <div class="form-group required">
                 {{Form::file('pet_image')}}
             </div>
         {{Form::hidden('_method', 'PUT')}}
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
         {!! Form::close() !!}
+        <div class="form-group required">
+          <label class='control-label'></label>Required.
+        </div>
 @endsection
