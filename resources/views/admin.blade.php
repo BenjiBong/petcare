cff0cb9148015934850191f36c1d9979e38b1f24
OneM <ben.bong95@gmail.com>
2017-09-28 16:08:49 +0800

    few changes to UI

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,25 +2,19 @@
 
 
 @section('content')
-
-
-
       <main class="col-sm-9 ml-sm-auto col-md-10 col-lg-8 pt-3" role="main">
         <div class="panel panel-default" >
           <div class="panel-heading" id="admin-panel"><h3>Admin Dashboard</h3></div>
-
           <div class="panel-body">
             <img src="/storage/profile_img/{{Auth::user()->avatar}}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
             <h3>{{ Auth::user()->name }}'s Profile</h3>
             {!! Form::open(['action'=>'AdminController@update', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
             <label>Update Profile Image</label>
+            {{--Form to update profile image--}}
             {{Form::file('profile_img')}}
             {{Form::submit('Submit', ['class'=>'pull-right btn btn-sm btn-primary'])}}
             {!! Form::close() !!}
           </div>
-
-
-
         </div>
       </main>
 
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,7 +8,7 @@
         <br><br>
     </div>
     <div class="col-lg-7 col-sm-6 col-xs-12">
-   
+
         <div><h1>{{$products->title}}</h1></div>
         <div class="info-orther">
          <div><p>{!!$products->body!!}</p></div>
@@ -25,19 +25,24 @@
     </div>
 
 
-            <a href="/products/{{$products->id}}/edit" class="btn btn-default">
-            <i class="fa fa-pencil-square" aria-hidden="true"></i>&nbsp;Edit
-            </a>
+
 
             <a href="{{route('product.addToCart', ['id' => $products->id])}}" class="btn btn-default">
             <i class="fa fa-shopping-cart"></i>&nbsp;Add Cart
             </a>
-        
+            <a href="/products" class="btn btn-default"><i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp;Go back</a>
+            @if (Auth::user())
+              @if (Auth::user()->getTable() == 'admins')
             {!!Form::open(['action' => ['ProductsController@destroy', $products->id], 'method' => 'POST', 'class' => 'pull-right' ])!!}
             {{Form::hidden('_method', 'DELETE')}}
             {{Form::submit('Delete', ['class' => 'btn btn-danger '])}}
             {!!Form::close()!!}
-            <a href="/products" class="btn btn-default"><i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp;Go back</a>
+            <a href="/products/{{$products->id}}/edit" class="btn pull-right btn-primary">
+            <i class="fa fa-pencil-square" aria-hidden="true"></i>&nbsp;Edit
+            </a>
+          @endif
+        @endif
+
             <br><br>
 
 </div>
