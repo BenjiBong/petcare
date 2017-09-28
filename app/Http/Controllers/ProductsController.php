860e46717c47e34dd1a90f3fe162dd888245990d
OneM <ben.bong95@gmail.com>
2017-09-28 11:46:55 +0800

    Enabled user to view and add to cart without loggin in

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -20,7 +20,7 @@ class ProductsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth', ['except' => ['index', 'show', 'getAddToCart', 'getCart']]);
     }
 
     public function getAddToCart(Request $request, $id){
